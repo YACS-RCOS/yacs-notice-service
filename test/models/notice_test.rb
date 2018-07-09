48c799ccdcbaacac09820b948d0bf336c77d1fce
gayj <gayj@rpi.edu>
2018-07-09 14:30:07 -0400

    fixed test cases, they all pass now!

--- test/models/notice_test.rb
+++ test/models/notice_test.rb
@@ -10,7 +10,7 @@ class NoticeTest < ActiveSupport::TestCase
     notice3 = Notice.create(start_date: Date. yesterday, end_date: 3.days.from_now) #good
     notice4 = Notice.create(start_date: 2.days.ago, end_date: Date.tomorrow) #good
 
-    assert_equals(Notice.current,[notice, notice2, notice3, notice4])
+    assert_equal(Notice.current,[notice, notice2, notice3, notice4])
   end
 
   test "current scope does not return reversed dates" do
@@ -18,20 +18,20 @@ class NoticeTest < ActiveSupport::TestCase
     notice1 = Notice.create(start_date: Date.tomorrow, end_date: Date.yesterday) #bad
     notice2 = Notice.create(start_date:5.days.from_now, end_date: Date.yesterday) #bad
 
-    assert_equals(Notice.current, nil)
+    assert_equal(Notice.current, [])
   end
 
   test "current scope does not return future dates" do
     notice = Notice.create(start_date: Date.tomorrow, end_date: 2.days.from_now)#bad
     notice2 = Notice.create(start_date: 4.days.from_now, end_date: 5.days.from_now) #bad
 
-    assert_equals(Notice.current, nil)
+    assert_equal(Notice.current, [])
   end
 
   test "current scope does not return old notices" do
     notice= Notice.create(start_date: 5.days.ago, end_date: Date.yesterday) #bad
     notice = Notice.create(start_date: 10.days.ago, end_date: 3.days.ago)
 
-    assert_equals(Notice.current, nil)
+    assert_equal(Notice.current, [])
   end
 end
