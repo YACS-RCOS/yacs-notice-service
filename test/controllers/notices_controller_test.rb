6a7ddfc218a0c91996d3a54f369e09c068cf5d05
gayj <gayj@rpi.edu>
2018-08-09 14:52:21 -0400

    Updated controller test by testing json response

--- test/controllers/notices_controller_test.rb
+++ test/controllers/notices_controller_test.rb
@@ -14,6 +14,7 @@ class NoticesControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Notice.count') do
       post notices_url, params: { notice: { end_date: @notice.end_date, message: @notice.message, start_date: @notice.start_date, alert_type: @notice.alert_type } }, as: :json
     end
+
     json_response = JSON.parse(response.body)
     assert_equal "Hello world!", json_response["message"]
     assert_response 201
@@ -28,9 +29,10 @@ class NoticesControllerTest < ActionDispatch::IntegrationTest
     patch notice_url(@notice), params: { notice: { end_date: @notice.end_date, message: "updated", start_date: @notice.start_date, alert_type: @notice.alert_type } }, as: :json
     @notice.reload
     assert_equal( @notice.message, "updated")
-    assert_response 200
+    
     json_response = JSON.parse(response.body)
     assert_equal "updated", json_response["message"]
+    assert_response 200
   end
 
   test "should destroy notice" do
